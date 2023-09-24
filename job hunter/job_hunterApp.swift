a655eb64701bd6dca001508ea3d8589cce7e79b6
qm994 <ma791778711@gmail.com>
2023-09-24 14:04:51 -0700

    Initial Commit

---
+++ job hunter/job_hunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  job_hunterApp.swift
+//  job hunter
+//
+//  Created by Qingyuan Ma on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct job_hunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
